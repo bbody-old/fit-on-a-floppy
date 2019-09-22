9eb923f4eca8822ec6be8aaf90851c69237047ff
Brendon Body <bbody.project@gmail.com>
2019-09-22 18:55:21 +1000

    Add favicon and loading

--- public/src/_scripts/main.js
+++ public/src/_scripts/main.js
@@ -9,26 +9,25 @@ addHelpers(Handlebars);
 
 window.onSubmit = function(event) {
   event.preventDefault();
-
-  var url = event.currentTarget[1].value;
-
+  var query = '#outputRow,#inputRow';
+  var url = event.currentTarget[0].value;
+  $(query).addClass('loading');
   $.ajax({                     
       type: "POST",                     
       url: 'https://7udyio7rpg.execute-api.us-west-2.amazonaws.com/default/pageSize',
       contentType: 'application/json',      
       data: JSON.stringify({
-          url,
-          protocol                
+          url
       }),                     
       success: function(res){
           var html = window.foaf.output(res);
           $('#output').html(html);
-          $('#outputRow').show();
+          $(query).removeClass('loading').addClass('loaded');
       },
       error: function(error){
         var html = window.foaf.error();
         $('#output').html(html);
-        $('#outputRow').show();
+        $(query).removeClass('loading').addClass('loaded');
       }
   }); 
 }
