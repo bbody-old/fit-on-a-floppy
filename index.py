6d28fda436e19f537bca3d6454ef846d43a5103d
Brendon Body <bbody.project@gmail.com>
2019-11-09 00:19:40 +1100

    Revert url and output

--- index.py
+++ index.py
@@ -19,5 +19,5 @@ def handler(event, context):
     return response
 
 if __name__ == '__main__':
-    event = {"body": "{\"url\": \"https://wwww.bonds.com.au\"}"}
+    event = {"body": "{\"url\": \"http://wwww.fitonafloppy.website\"}"}
     print(json.dumps(handler(event, None), indent = 4, sort_keys=True))
